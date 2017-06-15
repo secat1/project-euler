a2ac6d305b30106998b7d8b7b2854aa73cbcb6f8
secat1 <nuesleinad@hotmail.com>
2017-06-14 18:25:36 -0600

    Stick with function

--- problem1.py
+++ problem1.py
@@ -7,25 +7,6 @@ b = 5
 multiple_list = []
 
 i = 1
-'''
-while (a*i) < multiple:
-    if (b*i < multiple) and (b*i not in multiple_list):
-        multiple_list.append(b*i)
-        
-    if (a*i < multiple) and (a*i not in multiple_list):
-        multiple_list.append(a*i)
-          
-    i += 1
-
-
-# Compute sum of list content
-total = 0
-for i in multiple_list:
-    total = total + i
-
-print(total)
-
-'''
 
 def sum_multiple(multiple, a, b):
     multiple_list = []
