7f8860b4bae8721836655f439455867e1cdca428
secat1 <nuesleinad@hotmail.com>
2017-06-14 18:24:00 -0600

    Change to function

--- problem1.py
+++ problem1.py
@@ -7,7 +7,7 @@ b = 5
 multiple_list = []
 
 i = 1
-
+'''
 while (a*i) < multiple:
     if (b*i < multiple) and (b*i not in multiple_list):
         multiple_list.append(b*i)
@@ -16,8 +16,7 @@ while (a*i) < multiple:
         multiple_list.append(a*i)
           
     i += 1
-    
-print(multiple_list)
+
 
 # Compute sum of list content
 total = 0
@@ -25,3 +24,28 @@ for i in multiple_list:
     total = total + i
 
 print(total)
+
+'''
+
+def sum_multiple(multiple, a, b):
+    multiple_list = []
+    i = 1
+    
+    while (a*i) < multiple:
+        if (b*i < multiple) and (b*i not in multiple_list):
+            multiple_list.append(b*i)
+            
+        if (a*i < multiple) and (a*i not in multiple_list):
+            multiple_list.append(a*i)
+              
+        i += 1
+    
+    
+    # Compute sum of list content
+    total = 0
+    for i in multiple_list:
+        total = total + i
+    
+    print(f"The sum of all multiples of {a} or {b} less than {multiple} is: {total}") 
+    
+sum_multiple(1000, 3, 5)
